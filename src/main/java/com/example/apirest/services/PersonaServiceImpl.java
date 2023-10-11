0ab699186a312f5e2e052f50e0690d2f5967f6b9
Giothan <sergioalbino1080@gmail.com>
2023-10-11 15:13:48 -0300

    Api rest sin paginacion

--- src/main/java/com/example/apirest/services/PersonaServiceImpl.java
+++ src/main/java/com/example/apirest/services/PersonaServiceImpl.java
@@ -4,6 +4,8 @@ import com.example.apirest.entities.Persona;
 import com.example.apirest.repositories.BaseRepository;
 import com.example.apirest.repositories.PersonaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -30,4 +32,17 @@ public class PersonaServiceImpl extends BaseServiceImpl<Persona,Long> implements
             throw new Exception(e.getMessage());
         }
     }
+
+    @Override
+    public Page<Persona> search(String filtro, Pageable pageable) throws Exception {
+        try{
+            // Page<Persona> personas = personaRepository.findByNombreContainingOrApellidoContaining(filtro, filtro, pageable);
+            // Page<Persona> personas= personaRepository.search(filtro, pageable);
+            Page<Persona> personas= personaRepository.searchNativo(filtro, pageable);
+            return personas;
+        }catch (Exception e){
+            throw new Exception(e.getMessage());
+        }
+    }
+
 }
